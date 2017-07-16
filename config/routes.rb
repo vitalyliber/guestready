48b59704cd149dd407e25675b2375f55235bee54
Vitaly Liber <liber@sibext.com>
2017-07-16 18:23:25 +0700

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  get '*path', to: 'homes#index'
 
   namespace :api do
     namespace :v1 do
       resources :properties
     end
   end
+
+  get '*path', to: 'homes#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
